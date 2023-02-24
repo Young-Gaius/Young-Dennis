18e9e52bc654e8ee4c50784b5d7bbf3e44d61c8c
Dennis Kimutai <kimutaidennisbrian@gmail.com>
2023-02-24 16:52:25 +0300

    javascript

--- warm-up.js
+++ warm-up.js
@@ -1,13 +1,29 @@
 function catAndMouse(x, y, z) {
-    let catAPos = Math.abs(z -x);
-    let catBPos = Math.abs(z - y);
-     if (catAPos < catBPos){
-        return "Cat A";
-    }else if(catBPos < catAPos){
-        return "Cat B";
-    }else{
-        return "Mouse C"
+  let catAPos = Math.abs(z - x);
+  let catBPos = Math.abs(z - y);
+  if (catAPos < catBPos) {
+    return "Cat A";
+  } else if (catBPos < catAPos) {
+    return "Cat B";
+  } else {
+    return "Mouse C";
+  }
+}
+
+function breakingRecords(scores) {
+  let min = scores[0];
+  let max = scores[0];
+  let minCounter = 0;
+  let maxCounter = 0;
+  scores.forEach((value) => {
+    if (min > value) {
+      minCounter++;
+      min = value;
     }
-
-
+    if (max < value) {
+      maxCounter++;
+      max = value;
+    }
+  });
+  return [maxCounter, minCounter];
 }
